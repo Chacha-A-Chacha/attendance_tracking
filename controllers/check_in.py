accb10a5ca6bc4dd59560b569b5866b963f57372
Chacha Chacha <calex2607@gmail.com>
2025-02-26 15:31:32 +0300

    Initialize the system by importing the default data file

--- controllers/check_in.py
+++ controllers/check_in.py
@@ -37,6 +37,7 @@ def verify():
     
     return jsonify(result)
 
+
 @check_in_bp.route('/history/<unique_id>')
 def attendance_history(unique_id):
     """Get attendance history for a participant"""
@@ -44,6 +45,7 @@ def attendance_history(unique_id):
     result = verifier.get_participant_attendance_history(unique_id)
     
     return jsonify(result)
+
 
 @check_in_bp.route('/sessions')
 def available_sessions():
