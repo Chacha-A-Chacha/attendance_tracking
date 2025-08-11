f33e5937798df55847bd72d0555a44f04f904936
Chacha Chacha <calex2607@gmail.com>
2025-08-11 12:39:18 +0300

    User model with RBAC support.

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,21 @@
-# FILE: /attendance_tracking/models/__init__.py
-
+# models/__init__.py
+from .base import BaseModel
+from .user import User, Role, Permission, RoleType, user_roles
 from .participant import Participant
 from .session import Session
 from .attendance import Attendance
 from .session_reassignment import SessionReassignmentRequest, ReassignmentStatus
+
+__all__ = [
+    'BaseModel',
+    'User',
+    'Role',
+    'Permission',
+    'RoleType',
+    'user_roles',
+    'Participant',
+    'Session',
+    'Attendance',
+    'SessionReassignmentRequest',
+    'ReassignmentStatus'
+]
