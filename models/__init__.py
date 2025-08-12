fa01e227a6d29cba43b6b27e48bac1ec9dcc50f4
Chacha Chacha <calex2607@gmail.com>
2025-08-13 01:39:24 +0300

    extensions import

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 # models/__init__.py
-from app import db
 from .base import BaseModel
 from .user import User, Role, Permission, RoleType, user_roles
+from .enrollment import StudentEnrollment
 from .participant import Participant
 from .session import Session
 from .attendance import Attendance
@@ -10,6 +10,7 @@ from .session_reassignment import SessionReassignmentRequest, ReassignmentStatus
 __all__ = [
     'BaseModel',
     'User',
+    'StudentEnrollment',
     'Role',
     'Permission',
     'RoleType',
